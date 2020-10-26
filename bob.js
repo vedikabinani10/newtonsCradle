7219f97746f88a85d5467bc0d0e3be99086f708b
vedikabinani10 <69752144+vedikabinani10@users.noreply.github.com>
2020-10-26 21:33:16 +0530

    Update bob.js

--- bob.js
+++ bob.js
@@ -1,10 +1,9 @@
 class Bob{
     constructor(x,y){
         var options={
-            restitution : 0.3,
+            restitution : 1,
             friction : 0.5,
             density : 1.2,
-            isStatic : true
         }
         this.body = Bodies.circle(x,y,50,options);
         World.add(world,this.body);
