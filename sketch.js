25317c9fa9c8fe1e9a538278bbdbb956bf957414
vedikabinani10 <69752144+vedikabinani10@users.noreply.github.com>
2020-10-26 21:42:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,41 +4,37 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
+var engine, world;
 var roofObject;
 var bobObject1, bobObject2, bobObject3, bobObject4, bobObject5;
 var rope1, rope2, rope3, rope4, rope5;
 
-function preload()
-{
-	
-}
-
 function setup() {
-	var canvas = createCanvas(800, 700);
+	createCanvas(800, 700);
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
 	bobObject1 = new Bob(300,600);
-	bobObject2 = new Bob(350,600);
-	bobObject3 = new Bob(400,600);
-	bobObject4 = new Bob(450,600);
-	bobObject5 = new Bob(500,600);
+	bobObject2 = new Bob(400,600);
+	bobObject3 = new Bob(500,600);
+	bobObject4 = new Bob(600,600);
+	bobObject5 = new Bob(700,600);
 
 	roofObject = new Roof(400,400,250,20);
 
-	rope1 = new Rope(bobObject1.body,{x:300,y:400});
-	rope2 = new Rope(bobObject2.body,{x:350,y:400});
-	rope3 = new Rope(bobObject3.body,{x:400,y:400});
-	rope4 = new Rope(bobObject4.body,{x:450,y:400});
-	rope5 = new Rope(bobObject5.body,{x:500,y:400});
+	rope1 = new Rope(bobObject1.body,roofObject.body,-250,0);
+	rope2 = new Rope(bobObject2.body,roofObject.body,-150,0);
+	rope3 = new Rope(bobObject3.body,roofObject.body,-50,0);
+	rope4 = new Rope(bobObject4.body,roofObject.body,50,0);
+	rope5 = new Rope(bobObject5.body,roofObject.body,150,0);
   
 }
 
 
 function draw() {
-  rectMode(CENTER);
   background(200);
+  rectMode(CENTER);
   Engine.update(engine);
 
   bobObject1.display();
@@ -47,20 +43,17 @@ function draw() {
   bobObject4.display();
   bobObject5.display();
 
-  roofObject.display();
-
   rope1.display();
   rope2.display();
   rope3.display();
   rope4.display();
   rope5.display();
-
-  drawSprites();
-  keyPressed();
+	
+  roofObject.display();
 }
  function keyPressed(){
 	 if(keyCode===UP_ARROW){
-		 Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-5,y:-5});
+		 Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-200,y:0});
 	 }
  }
 
